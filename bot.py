c77499c9015a067ac871d6bf978bd62ceaa6e2e2
Vinicius Silva Cruz <cruztheripper@gmail.com>
2025-02-27 14:02:08 -0300

    aa

--- bot.py
+++ bot.py
@@ -88,6 +88,13 @@ def carregar_xpPorNivel():
     with open("xpPorNivel.json", 'r') as f:
         return json.load(f)
 
+def carregar_ataques():
+    with open("ataques.json", 'r') as f:
+        return json.load(f)
+
+def atualizar_ataques(dados):
+    with open("ataques.json", 'w') as f:
+        json.dump(dados, f, indent=4)
 #comandos
 
 @bot.event
@@ -171,6 +178,7 @@ async def rolar_pericia(ctx, pericia):
     await ctx.send(f"Resultado: {resultado} \nRolagem -> {dado} + Bônus -> {pericias[str(bot.user.id)][pericia]}")
 
 @bot.command(name="guardar")
+@commands.has_permissions(administrator=True) 
 async def guardar_item(ctx, item, *, descricao):
     inventario = carregar_inventario()
 
@@ -185,6 +193,7 @@ async def guardar_item(ctx, item, *, descricao):
     await ctx.send(f"Item '{item}' guardado com sucesso!")
 
 @bot.command(name='remover')
+@commands.has_permissions(administrator=True) 
 async def remover_item(ctx, item):
     inventario = carregar_inventario()
     if bot.user.id not in inventario:
@@ -194,7 +203,8 @@ async def remover_item(ctx, item):
         atualizar_inventario(inventario)
         await ctx.send(f"{item} removido com sucesso!")
 
-@bot.command(name='novopoder')
+@bot.command(name='npoder')
+@commands.has_permissions(administrator=True) 
 async def novo_poder(ctx, poder, *, descricao):
     poderes = carregar_poderes_e_habilidades()
     bot_id = str(bot.user.id)
@@ -204,7 +214,8 @@ async def novo_poder(ctx, poder, *, descricao):
     atualizar_poderes_e_habilidades(poderes)
     await ctx.send(f"Poder {poder} adicionado com sucesso!")
 
-@bot.command(name='removerpoder')
+@bot.command(name='rpoder')
+@commands.has_permissions(administrator=True) 
 async def remover_poder(ctx, poder):
     poderes = carregar_poderes_e_habilidades()
     bot_id = str(bot.user.id)
@@ -215,7 +226,8 @@ async def remover_poder(ctx, poder):
         atualizar_poderes_e_habilidades(poderes)
         await ctx.send(f"Poder {poder} removido com sucesso!")
 
-@bot.command(name='aprendermagia')
+@bot.command(name='amagia')
+@commands.has_permissions(administrator=True) 
 async def aprender_magia(ctx, magia, custo, *, descricao):
     magias = carregar_magias()
     bot_id = str(bot.user.id)
@@ -226,6 +238,7 @@ async def aprender_magia(ctx, magia, custo, *, descricao):
     await ctx.send(f"Magia {magia} adicionada com sucesso!")
 
 @bot.command(name='esquecermagia')
+@commands.has_permissions(administrator=True) 
 async def esquecer_magia(ctx, magia):
     magias = carregar_magias()
     bot_id = str(bot.user.id)
@@ -236,7 +249,7 @@ async def esquecer_magia(ctx, magia):
         atualizar_magias(magias)
         await ctx.send(f"Magia {magia} esquecida com sucesso!")
 
-@bot.command(name='verinventario')
+@bot.command(name='inventario')
 async def ver_inventario(ctx):
     inventario = carregar_inventario()
     usuario = str(bot.user.id)
@@ -258,42 +271,208 @@ async def ver_inventario(ctx):
 
 @bot.command(name='status')
 async def status(ctx):
-    # Carrega os dados de status do usuário
     status = carregar_status()
     user_id = str(bot.user.id)
     
+    vidaMaxima = status[user_id]["pvMax"]
+    manaMaxima = status[user_id]["pmMax"]
+    vidaAtual = status[user_id]["pv"]
+    manaAtual = status[user_id]["pm"]
+
     if user_id not in status:
         await ctx.send("Você não tem status registrado!")
         return
 
-    # Recupera os dados de XP e nível do usuário
+    
     dados_usuario = status[user_id]
     xp_atual = dados_usuario['xp']
     nivel_atual = dados_usuario['nivel']
     xp_por_nivel = carregar_xpPorNivel()
+    xp_por_nivel = {int(k): v for k, v in xp_por_nivel.items()}
     # Determina o XP necessário para o próximo nível
-    xp_para_proximo_nivel = xp_por_nivel.get(nivel_atual + 1, None)
+    xp_para_proximo_nivel = xp_por_nivel.get(nivel_atual + 1)
     
     if xp_para_proximo_nivel is None:
         await ctx.send("Você já atingiu o nível máximo!")
         return
 
-    # Calcula o progresso
+    
     progresso = (xp_atual - xp_por_nivel[nivel_atual]) / (xp_para_proximo_nivel - xp_por_nivel[nivel_atual]) * 100
     progresso = round(progresso)
 
-    # Criando a barra de XP visual
+   
     blocos_cheios = int(progresso / 10)
     blocos_vazios = 10 - blocos_cheios
     barra_xp = "🟩" * blocos_cheios + "⬛" * blocos_vazios
+     # Barra de Vida
+    vida_porcentagem = (vidaAtual / vidaMaxima) * 100
+    blocos_cheios_vida = int(vida_porcentagem / 10)
+    blocos_vazios_vida = 10 - blocos_cheios_vida
+    barra_vida = "🟥" * blocos_cheios_vida + "⬛" * blocos_vazios_vida
 
-    # Envia o Embed com a barra de XP
-    embed = discord.Embed(
-        title=f"Status - Nível {nivel_atual}",
-        description=f"{barra_xp} {progresso}%\n\nXP Atual: {xp_atual}/{xp_para_proximo_nivel}\nPróximo Nível: {nivel_atual + 1}",
-        color=0x00FF00
+    # Barra de Mana
+    mana_porcentagem = (manaAtual / manaMaxima) * 100
+    blocos_cheios_mana = int(mana_porcentagem / 10)
+    blocos_vazios_mana = 10 - blocos_cheios_mana
+    barra_mana = "🟦" * blocos_cheios_mana + "⬛" * blocos_vazios_mana
+
+    embed = discord.Embed(title="📊 Estatísticas do Borim 📊", color=discord.Color.blue())
+   
+    embed.add_field(
+    name="❤️ Vida",
+    value=f"{barra_vida} {vidaAtual}/{vidaMaxima} HP\n\n"  
     )
 
+    embed.add_field(
+    name="🔵 Mana",
+    value=f"{barra_mana} {manaAtual}/{manaMaxima} MP\n\n"  
+    )
+
+    embed.add_field(
+    name="📈 Progresso de XP",
+    value=f"{barra_xp} {progresso}%\nXP Atual: {xp_atual}/{xp_para_proximo_nivel}\nPróximo Nível: {nivel_atual + 1}\n\n"  
+    )
     await ctx.send(embed=embed)
 
+@bot.command(name='+pv')
+
+async def aumentar_pv(ctx, valor):
+    status = carregar_status()
+    user_id = str(bot.user.id)
+    if user_id not in status:
+        status[user_id] = {}
+    status[user_id]["pv"] += int(valor)
+    atualizar_status(status)
+    await ctx.send(f"Vida aumentada em {valor} pontos!")
+
+@bot.command(name='-pv')
+async def diminuir_pv(ctx, valor):
+    status = carregar_status()
+    user_id = str(bot.user.id)
+    if user_id not in status:
+        status[user_id] = {}
+    status[user_id]["pv"] -= int(valor)
+    atualizar_status(status)
+    await ctx.send(f"Vida diminuída em {valor} pontos!")
+
+@bot.command(name='+pm')
+async def aumentar_pm(ctx, valor):
+    status = carregar_status()
+    user_id = str(bot.user.id)
+    if user_id not in status:
+        status[user_id] = {}
+    status[user_id]["pm"] += int(valor)
+    atualizar_status(status)
+    await ctx.send(f"Mana aumentada em {valor} pontos!")
+
+@bot.command(name='-pm')
+async def diminuir_pm(ctx, valor):
+    status = carregar_status()
+    user_id = str(bot.user.id)
+    if user_id not in status:
+        status[user_id] = {}
+    status[user_id]["pm"] -= int(valor)
+    atualizar_status(status)
+    await ctx.send(f"Mana diminuída em {valor} pontos!")
+
+@bot.command(name="+xp")
+async def adicionar_xp(ctx, valor):
+    status = carregar_status()
+    user_id = str(bot.user.id)
+    if user_id not in status:
+        status[user_id] = {}
+    status[user_id]["xp"] += int(valor)
+    atualizar_status(status)
+    await ctx.send(f"XP aumentado em {valor} pontos!")
+
+@bot.command(name="poderes")
+
+async def ver_poderes(ctx):
+    poderes = carregar_poderes_e_habilidades()
+    usuario = str(bot.user.id)
+    if usuario not in poderes:
+        poderes[usuario] = {}
+    embed = discord.Embed(title="🔮 Poderes e Habilidades do Borim 🔮", color=discord.Color.blue())
+
+    if usuario in poderes and len(poderes[usuario]) > 0:    
+        for poder, descricao in poderes[usuario].items():
+            embed.add_field(name=f"👉 {poder}", value=f"📝 Descrição: {descricao}\n\n", inline=False)
+    else:
+         embed.add_field(
+            name="🛑 Sem poderes",
+            value="Você não tem poderes e habilidades ainda. 🏚️",
+            inline=False
+        )
+    await ctx.send(embed=embed)
+
+@bot.command(name="magias")
+async def ver_magias(ctx):
+    magias = carregar_magias()
+    usuario = str(bot.user.id)
+    if usuario not in magias:
+        magias[usuario] = {}
+    embed = discord.Embed(title="🔮 Magias do Borim 🔮", color=discord.Color.blue())
+
+    if usuario in magias and len(magias[usuario]) > 0:    
+        for magia, dados in magias[usuario].items():
+            embed.add_field(name=f"👉 {magia}", value=f"📝 Custo: {dados['custo']}\n📝 Descrição: {dados['descricao']}\n\n", inline=False)
+    else:
+         embed.add_field(
+            name="🛑 Sem magias",
+            value="Você não tem magias ainda. 🏚️",
+            inline=False
+        )
+    await ctx.send(embed=embed)
+
+@bot.command(name="cataque")
+@commands.has_permissions(administrator=True) 
+async def criar_ataque(ctx,ataque, modificador, dano, *, descricao):
+    ataques = carregar_ataques()
+    bot_id = str(bot.user.id)
+    if bot_id not in ataques:
+        ataques[bot_id] = {}
+    ataques[bot_id][ataque] = {"modificador": modificador, "descricao": descricao, "dano": dano}
+    atualizar_ataques(ataques)
+    await ctx.send(f"Ataque {ataque} adicionado com sucesso!")
+
+@bot.command(name="remataque")
+@commands.has_permissions(administrator=True) 
+async def remover_ataque(ctx, ataque):
+    ataques = carregar_ataques()
+    bot_id = str(bot.user.id)
+    if bot_id not in ataques:
+        ataques[bot_id] = {}
+    if ataque in ataques[bot_id]:
+        ataques[bot_id].pop(ataque)
+        atualizar_ataques(ataques)
+        await ctx.send(f"Ataque {ataque} removido com sucesso!")
+
+@bot.command(name="ataques")
+async def ver_ataques(ctx):
+    ataques = carregar_ataques()
+    usuario = str(bot.user.id)
+    if usuario not in ataques:
+        ataques[usuario] = {}
+    embed = discord.Embed(title="⚔️ Ataques do Borim ⚔️", color=discord.Color.blue())
+
+    if usuario in ataques and len(ataques[usuario]) > 0:    
+        for ataque, dados in ataques[usuario].items():
+            embed.add_field(name=f"👉 {ataque}", value=f"📝 Modificador: {dados['modificador']} \n 📝 Dano: {dados['dano']}\n 📝 Descrição: {dados['descricao']}\\n\n", inline=False)
+    else:
+         embed.add_field(
+            name="🛑 Sem ataques",
+            value="Você não tem ataques ainda. 🏚️",
+            inline=False
+        )
+    await ctx.send(embed=embed)
+
+@bot.command(name="rataque")
+async def rolar_ataque(ctx, ataque):
+    ataques = carregar_ataques()
+    dados = ataques[str(bot.user.id)][ataque]
+    dado = random.randint(1, 20)
+    resultado =  dado + int(dados["modificador"])
+    await ctx.send(f"Resultado: {resultado} \nRolagem -> {dado} + Modificador -> {dados['modificador']}")
+
+    
 bot.run(TOKEN)
